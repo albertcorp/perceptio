5e0aa7ab56266f3efc64bfd9b29b8ab8354e5f81
albert jonathan <albertcorp@gmail.com>
2019-07-02 20:58:52 +0000

    Commit 1

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("Perceptio.Ejercicio1.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Perceptio/Ejercicio1/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
